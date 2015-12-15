c74285d2331475d8bf10a46e25e3b5d0de3e7ea5
Satria Agung M <satryaway@gmail.com>
2015-12-15 21:47:36 +0700

    Functional - Registration

--- app/src/main/java/com/jixstreet/spaace/SignUpActivity.java
+++ app/src/main/java/com/jixstreet/spaace/SignUpActivity.java
@@ -6,9 +6,20 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.jixstreet.spaace.utils.CommonConstants;
 import com.jixstreet.spaace.utils.Utility;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
 
 /**
  * Created by satryaway on 12/11/2015.
@@ -21,6 +32,7 @@ public class SignUpActivity extends BaseActivity {
     private EditText passwordET;
     private TextView signUpTV;
     private TextView messageTV;
+    private ProgressBar loadingPB;
 
     @Override
     public int setView() {
@@ -42,7 +54,7 @@ public class SignUpActivity extends BaseActivity {
         passwordET = (EditText) findViewById(R.id.password_et);
         signUpTV = (TextView) findViewById(R.id.signup_tv);
         messageTV = (TextView) findViewById(R.id.message_tv);
-    }
+        loadingPB = (ProgressBar) findViewById(R.id.loading_pb);    }
 
     @Override
     public void setCallBack() {
@@ -89,7 +101,55 @@ public class SignUpActivity extends BaseActivity {
     }
 
     private void doRegister() {
+        String url = CommonConstants.SERVICE_SIGN_UP;
 
+        RequestParams requestParams = new RequestParams();
+        requestParams.put(CommonConstants.EMAIL, emailET.getText().toString());
+        requestParams.put(CommonConstants.PASSWORD, passwordET.getText().toString());
+        requestParams.put(CommonConstants.GRANT_TYPE, CommonConstants.PASSWORD);
+        requestParams.put(CommonConstants.ATTRIBUTE_FULL_NAME, CommonConstants.PASSWORD);
+
+        AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
+        asyncHttpClient.addHeader(CommonConstants.API_TOKEN, getString(R.string.api_token_header));
+        asyncHttpClient.post(url, requestParams, new JsonHttpResponseHandler(){
+            @Override
+            public void onStart() {
+                signUpTV.setVisibility(View.INVISIBLE);
+                loadingPB.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onFinish() {
+                loadingPB.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                signUpTV.setVisibility(View.VISIBLE);
+                signUpTV.setText(R.string.Succeed);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                Toast.makeText(SignUpActivity.this, R.string.RTO, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                signUpTV.setVisibility(View.VISIBLE);
+                signUpTV.setText(R.string.register_failed);
+                signUpTV.setBackgroundColor(getResources().getColor(R.color.failed_red));
+                messageTV.setVisibility(View.VISIBLE);
+
+                try {
+                    JSONObject meta = errorResponse.getJSONObject(CommonConstants.META);
+                    messageTV.setText(meta.getString(CommonConstants.ERROR_DESCRIPTION));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
     }
 
     private boolean isFormFilled() {
